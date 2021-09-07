852a5774f527b0d6f44855db04e08b63e302b561
vicky <vicky99_perez@hotmail.com>
2021-09-06 22:22:05 -0300

    Primera parte del programa

---
+++ Programatp.php
@@ -0,0 +1,26 @@
+<?php
+$Factorial = rand(1,20);
+
+function Factorialde($Digito)
+{
+    $Mul = 1;
+
+    for($i=$Digito; $i > 0; $i--)
+    {
+        $Mul = $Mul * $i;
+    } 
+    echo $Mul. "<br>";
+    echo $Digito. "<br>";
+}
+function AlCubo($Digito)
+{
+    for($i=1; $i <= 3, $i++)
+    {
+        $Prod = $Prod * $Digito;
+    }
+    echo $Prod;
+}
+
+Factorialde($Factorial);
+
+?>
